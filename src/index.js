d27f4450cb5f2b9af2d0e66cdd123bdfae7f6fef
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-04 00:01:22 +0530

    Create div element container with two child element

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//parent element 
+//properties 
+//childElement
 
-const element = React.createElement('h1', null, 'Hellow  world');
+const element = React.createElement('div', { className: "first-element" },
+  React.createElement('h1', null, "hello world !"),
+  React.createElement("h3", { className: 'true' }, "hi, TheAgunda...")
+);
 
 ReactDOM.render(element, document.getElementById('application'));
