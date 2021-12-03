72f4879c541e6d8ac68995b39b5a8e328c29cd90
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-03 23:49:39 +0530

    Adding style in element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-const first_element = <h1>This is first element</h1>;
+const first_element = <h1 className="first-element">This is first element</h1>;
 
 ReactDOM.render(first_element, document.getElementById('application'));
