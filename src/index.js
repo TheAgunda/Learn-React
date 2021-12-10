65d3f752787b8073ceda0516ae20b75f0b41bd96
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-10 22:11:55 +0530

    Display data with the help of array

--- src/index.js
+++ src/index.js
@@ -1,60 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import PropTypes from 'prop-types'
 
-//Context provides a way to pass data through the component tree without having to pass props down manually at every level.
-
-//Context is primarily used when some data needs to be accessible by many components at different nesting levels.
-
-
-const employeeContext = React.createContext();
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Id: 100,
-      Name: "Kiran Badola",
-      Location: "India",
-      Salary: "1000K"
-    }
-  }
-  change_employee_data = () => {
-    this.setState({ Id: 103 })
-  }
-  render() {
-    return <div>
-
-      <p>This is App component</p>
-      Employee ID:  {this.state.Id}
-      <employeeContext.Provider value={this.state}>
-        <Employee></Employee>
-      </employeeContext.Provider>
-      <button onClick={this.change_employee_data}>Change employee data</button>
-    </div>
-  }
-
+function Employee(props) {
+  return <div style={{ border: "1px solid red" }}>
+    <p>
+      Employee ID {props.meta.id}
+    </p>
+    <p>
+      Employee Name {props.meta.name}
+    </p>
+    <p>
+      Employee Salary {props.meta.salary}
+    </p>
+    <p>
+      Employee Location {props.meta.location}
+    </p>
+  </div>
 }
 
-class Employee extends React.Component {
-  static context = employeeContext;
-  render() {
-    return <div>
-      <p>This is Employee component</p>
-      Employee ID {this.context.Id}
-      <Salary></Salary>
-    </div>
-  }
+function DisplayEmployee(params) {
+  const empList = params.employeeList;
+  console.log(empList);
+  const listElement = empList.map((emp) =>
+    <Employee key={emp.id} meta={emp}></Employee>
+  );
+  return (<div>
+    {listElement}
+  </div>);
 }
-class Salary extends React.Component {
-  static context = employeeContext;
-  render() {
-    return <div>
-      <p>This is Salary component</p>
-      Employee ID {this.context.Name}
-    </div>
-  }
-}
-const elements = <App></App>
 
-ReactDOM.render(elements, document.getElementById('application'))
+const employee = [
+  { id: 20, name: 'Kiran', location: 'India', salary: '2000k' },
+  { id: 25, name: 'Sumit', location: 'India', salary: '2000k' },
+  { id: 24, name: 'Rahul', location: 'India', salary: '2000k' },
+]
+
+const element = <DisplayEmployee employeeList={employee} ></DisplayEmployee>;
+ReactDOM.render(element, document.getElementById("application"));
