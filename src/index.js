de2c510ee9d31b5254e15c4e52d4c336d29e0586
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-12 00:40:25 +0530

    Call REST API from React

--- src/index.js
+++ src/index.js
@@ -1,39 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Employee(props) {
-  return <div style={{ border: "1px solid red" }}>
-    <p>
-      Employee ID {props.meta.id}
-    </p>
-    <p>
-      Employee Name {props.meta.name}
-    </p>
-    <p>
-      Employee Salary {props.meta.salary}
-    </p>
-    <p>
-      Employee Location {props.meta.location}
-    </p>
-  </div>
+class EmployeeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: [],
+    }
+  }
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/api/employees').then(response => response.json())
+      .then(result => {
+        this.setState({ employees: result.employees })
+        console.log(result.employees)
+      }
+      )
+  }
+  render() {
+    return <div>
+      <div className='container p-4'>
+        <p>Employees DataBase</p>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Location</th>
+              <th scope="col">Salary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map(emp => (
+              <tr key={emp.id}>
+                <th scope="row">{emp.id}</th>
+                <td>{emp.name}</td>
+                <td>{emp.location}</td>
+                <td>{emp.salary}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  }
 }
+const element = <EmployeeComponent></EmployeeComponent>;
 
-function DisplayEmployee(params) {
-  const empList = params.employeeList;
-  console.log(empList);
-  const listElement = empList.map((emp) =>
-    <Employee key={emp.id} meta={emp}></Employee>
-  );
-  return (<div>
-    {listElement}
-  </div>);
-}
-
-const employee = [
-  { id: 20, name: 'Kiran', location: 'India', salary: '2000k' },
-  { id: 25, name: 'Sumit', location: 'India', salary: '2000k' },
-  { id: 24, name: 'Rahul', location: 'India', salary: '2000k' },
-]
-
-const element = <DisplayEmployee employeeList={employee} ></DisplayEmployee>;
 ReactDOM.render(element, document.getElementById("application"));
