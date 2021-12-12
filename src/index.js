3fe38c2fb7be8c6952d6b809f7e212d2f790fb0d
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-13 00:39:09 +0530

    Controlled Components in react

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ class EmployeeComponent extends React.Component {
     super(props);
     this.state = {
       employees: [],
+      employee: {
+        name: '',
+        salary: '',
+        location: '',
+      },
       message: '',
     }
   }
@@ -15,12 +20,18 @@ class EmployeeComponent extends React.Component {
         this.setState({ employees: result.employees })
       });
   }
+  inputChangeEventHandeler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      employee: {
+        ...this.state.employee,
+        [name]: value,
+      }
+    })
+  }
   createEmployee = () => {
-    let emp = {
-      name: this.refs.name.value,
-      location: this.refs.location.value,
-      salary: this.refs.salary.value,
-    };
+    let emp = this.state.employee;
     fetch('http://127.0.0.1:8000/api/employees', {
       method: 'POST',
       headers: {
@@ -44,48 +55,56 @@ class EmployeeComponent extends React.Component {
   render() {
     return <div>
       <div className='container p-4'>
-        <div className='container'>
-          <div className="mb-3">
-            <label className="form-label">Name</label>
-            <input type="text" className="form-control" ref="name" placeholder="name" />
+        <div className='row mb-4'>
+          <div className='col-md-4'>
+            <div className='card'>
+              <div className='card-body'>
+                <h5>Create New Employee</h5>
+                <div className="mb-3">
+                  <label className="form-label">Name</label>
+                  <input type="text" className="form-control" name="name" placeholder="name" value={this.state.employee.name} onChange={this.inputChangeEventHandeler} />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Location</label>
+                  <input type="text" className="form-control" name="location" placeholder="location" value={this.state.employee.location} onChange={this.inputChangeEventHandeler} />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Salary</label>
+                  <input type="number" className="form-control" name="salary" placeholder="salary" value={this.state.employee.salary} onChange={this.inputChangeEventHandeler} />
+                </div>
+                <button onClick={this.createEmployee} className='btn btn-info'>
+                  Create
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="mb-3">
-            <label className="form-label">Location</label>
-            <input type="text" className="form-control" ref="location" placeholder="location" />
+          <div className='col-md-8'>
+            <p>Employees DataBase</p>
+            {this.state.message}
+            <table className='table table-striped'>
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Location</th>
+                  <th scope="col">Salary</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.employees.map(emp => (
+                  <tr key={emp.id}>
+                    <th scope="row">{emp.id}</th>
+                    <td>{emp.name}</td>
+                    <td>{emp.location}</td>
+                    <td>{emp.salary}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-          <div className="mb-3">
-            <label className="form-label">Salary</label>
-            <input type="number" className="form-control" ref="salary" placeholder="salary" />
-          </div>
-          <button onClick={this.createEmployee} className='btn btn-info'>
-            Create
-          </button>
-
         </div>
-        {this.state.message}
-        <p>Employees DataBase</p>
-        <table className='table table-striped'>
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Location</th>
-              <th scope="col">Salary</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.employees.map(emp => (
-              <tr key={emp.id}>
-                <th scope="row">{emp.id}</th>
-                <td>{emp.name}</td>
-                <td>{emp.location}</td>
-                <td>{emp.salary}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       </div>
-    </div>
+    </div >
   }
 }
 const element = <EmployeeComponent></EmployeeComponent>;
