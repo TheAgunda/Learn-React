4fcf5ad161a7bff3d4c650a76e375da2aea988e5
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-12 22:18:34 +0530

    Create employee using api

--- src/index.js
+++ src/index.js
@@ -6,19 +6,63 @@ class EmployeeComponent extends React.Component {
     super(props);
     this.state = {
       employees: [],
+      message: '',
     }
   }
   componentDidMount() {
     fetch('http://127.0.0.1:8000/api/employees').then(response => response.json())
       .then(result => {
         this.setState({ employees: result.employees })
-        console.log(result.employees)
-      }
-      )
+      });
+  }
+  createEmployee = () => {
+    let emp = {
+      name: this.refs.name.value,
+      location: this.refs.location.value,
+      salary: this.refs.salary.value,
+    };
+    fetch('http://127.0.0.1:8000/api/employees', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'mode': 'cors',
+      },
+      body: JSON.stringify(emp),
+    }).then(response => response.json())
+      .then(result => {
+        if (result) {
+          this.setState({ message: result.message })
+          fetch('http://127.0.0.1:8000/api/employees').then(response => response.json())
+            .then(result => {
+              this.setState({ employees: result.employees })
+            });
+        }
+      });
   }
   render() {
     return <div>
       <div className='container p-4'>
+        <div className='container'>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input type="text" className="form-control" ref="name" placeholder="name" />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Location</label>
+            <input type="text" className="form-control" ref="location" placeholder="location" />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Salary</label>
+            <input type="number" className="form-control" ref="salary" placeholder="salary" />
+          </div>
+          <button onClick={this.createEmployee} className='btn btn-info'>
+            Create
+          </button>
+
+        </div>
+        {this.state.message}
         <p>Employees DataBase</p>
         <table className='table table-striped'>
           <thead>
