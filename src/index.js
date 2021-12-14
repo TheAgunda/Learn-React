53ae27779fbe143849d427bd96949740c7310178
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-14 23:57:33 +0530

    use formik package

--- src/index.js
+++ src/index.js
@@ -1,112 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useFormik } from 'formik';
 
-class EmployeeComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employees: [],
-      employee: {
-        name: '',
-        salary: '',
-        location: '',
-      },
-      message: '',
+const EmployeeComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      salary: '',
+      location: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values))
     }
-  }
-  componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/employees').then(response => response.json())
-      .then(result => {
-        this.setState({ employees: result.employees })
-      });
-  }
-  inputChangeEventHandeler = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({
-      employee: {
-        ...this.state.employee,
-        [name]: value,
-      }
-    })
-  }
-  createEmployee = () => {
-    let emp = this.state.employee;
-    fetch('http://127.0.0.1:8000/api/employees', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'mode': 'cors',
-      },
-      body: JSON.stringify(emp),
-    }).then(response => response.json())
-      .then(result => {
-        if (result) {
-          this.setState({ message: result.message })
-          fetch('http://127.0.0.1:8000/api/employees').then(response => response.json())
-            .then(result => {
-              this.setState({ employees: result.employees })
-            });
-        }
-      });
-  }
-  render() {
-    return <div>
-      <div className='container p-4'>
-        <div className='row mb-4'>
-          <div className='col-md-4'>
-            <div className='card'>
-              <div className='card-body'>
-                <h5>Create New Employee</h5>
+
+  })
+  return (
+    <div className='container p-4'>
+      <div className='row mb-4'>
+        <div className='col-md-4'>
+          <div className='card'>
+            <div className='card-body'>
+              <h5>Create New Employee</h5>
+              <form onSubmit={formik.handleSubmit}>
                 <div className="mb-3">
-                  <label className="form-label">Name</label>
-                  <input type="text" className="form-control" name="name" placeholder="name" value={this.state.employee.name} onChange={this.inputChangeEventHandeler} />
+                  <label className="form-label" htmlFor='name'>Name</label>
+                  <input type="text" className="form-control" name="name" id="name" placeholder="name" value={formik.values.name} onChange={formik.handleChange} />
                 </div>
                 <div className="mb-3">
-                  <label className="form-label">Location</label>
-                  <input type="text" className="form-control" name="location" placeholder="location" value={this.state.employee.location} onChange={this.inputChangeEventHandeler} />
+                  <label className="form-label" htmlFor='location'>Location</label>
+                  <input type="text" className="form-control" name="location" id="location" placeholder="location" value={formik.values.location} onChange={formik.handleChange} />
                 </div>
                 <div className="mb-3">
-                  <label className="form-label">Salary</label>
-                  <input type="number" className="form-control" name="salary" placeholder="salary" value={this.state.employee.salary} onChange={this.inputChangeEventHandeler} />
+                  <label className="form-label" htmlFor=' q'>Salary</label>
+                  <input type="number" className="form-control" name="salary" id="salary" placeholder="salary" value={formik.values.salary} onChange={formik.handleChange} />
                 </div>
-                <button onClick={this.createEmployee} className='btn btn-info'>
+                <button className='btn btn-info'>
                   Create
                 </button>
-              </div>
+              </form>
             </div>
-          </div>
-          <div className='col-md-8'>
-            <p>Employees DataBase</p>
-            {this.state.message}
-            <table className='table table-striped'>
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Location</th>
-                  <th scope="col">Salary</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.employees.map(emp => (
-                  <tr key={emp.id}>
-                    <th scope="row">{emp.id}</th>
-                    <td>{emp.name}</td>
-                    <td>{emp.location}</td>
-                    <td>{emp.salary}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
           </div>
         </div>
       </div>
-    </div >
-  }
+    </div>
+  )
 }
+
 const element = <EmployeeComponent></EmployeeComponent>;
 
 ReactDOM.render(element, document.getElementById("application"));
