88efe9ec2a922b1ccc51228e4e9cba1d90cf23e5
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-17 23:40:03 +0530

    use formik form errormessage and field component of formik lib

--- src/index.js
+++ src/index.js
@@ -1,68 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useFormik } from 'formik';
+import { ErrorMessage, Field, Form, Formik, useFormik } from 'formik';
 import * as yup from 'yup';
 
-
 const EmployeeComponent = () => {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      salary: '',
-      location: '',
-      email: '',
-    },
-    validationSchema: yup.object({
-      name:yup.string(20,"name should not exceed 20 character").required("this is requied value"),
-      location:yup.string().required("this is requied value"),
-      email:yup.string(20,"name should not exceed 20 character").email("invalid emai").required("this is requied value"),
-    }),
-    onSubmit: values => {
-      alert(JSON.stringify(values))
-    }
 
-  })
 
   //Validation Rules
   return (
-    <div className='container p-4'>
-      <div className='row mb-4'>
-        <div className='col-md-4'>
-          <div className='card'>
-            <div className='card-body'>
-              <h5>Create New Employee</h5>
-              <form onSubmit={formik.handleSubmit}>
-                <div className="mb-3">
-                  <label className="form-label" htmlFor='name'>Name</label>
-                  <input  name="name" {...formik.getFieldProps('name')} />
+    <Formik
+      initialValues={{
+        name: '',
+        salary: '',
+        location: '',
+        email: '',
+        designation: ''
+      }}
+
+      validationSchema={yup.object({
+        name: yup.string(20, "name should not exceed 20 character").required("this is requied value"),
+        location: yup.string().required("this is requied value"),
+        email: yup.string(20, "name should not exceed 20 character").email("invalid emai").required("this is requied value"),
+      })}
 
 
+      onSubmit={values => {
+        alert(JSON.stringify(values))
+      }}>
 
-                  {formik.touched.name && formik.errors.name ? <div class="invalid-feedback d-flex">{formik.errors.name}</div> : null}
-                </div>
-                <div className="mb-3">
-                  <label className="form-label" htmlFor='email'>Email</label>
-                  <input name="email" {...formik.getFieldProps('email')}/>
+      {
+        props => (
+          <div>
+            <h2>New Employee Form</h2>
+            <Form>
+              <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
+                <Field name="name" type="text"></Field>
+                <ErrorMessage name='name'></ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Salary</label>
+                <Field name="salary" type="text"></Field>
+                <ErrorMessage name='salary'></ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Location</label>
+                <Field name="location" type="text"></Field>
+                <ErrorMessage name='location'></ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
+                <Field name="email" type="email"></Field>
+                <ErrorMessage name='email'></ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Designation</label>
+                <Field name="designation" as="select">
+                  <option values="software-engg">Software Engg.</option>
+                  <option values="programmer">Programmer</option>
+                  <option values="tester">Tester</option>
+                </Field>
 
-                  {formik.touched.email && formik.errors.email ? <div class="invalid-feedback d-flex">{formik.errors.email}</div> : null}
-                </div>
-                <div className="mb-3">
-                  <label className="form-label" htmlFor='location'>Location</label>
-                  <input  name="location" {...formik.getFieldProps('location')} />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label" htmlFor='salary'>Salary</label>
-                  <input name="salary" {...formik.getFieldProps('salary')} />
-                </div>
-                <button className='btn btn-info'>
-                  Create
-                </button>
-              </form>
-            </div>
+              </div>
+              <button type='submit' disabled={!props.isValid}>Create</button>
+            </Form>
           </div>
-        </div>
-      </div>
-    </div>
+        )
+      }
+    </Formik >
   )
 }
 
