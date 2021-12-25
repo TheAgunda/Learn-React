e7fb86c720513e2e0d92581dc1cdea7fd9604615
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-25 23:35:13 +0530

    Higher Order Components in React

--- src/index.js
+++ src/index.js
@@ -3,87 +3,185 @@ import React from "react";
 import reactDom from "react-dom";
 
 
-//Refs in React 
-//Refs Forwarding
-//Using ref in functional and class based component
 
-const DemoComponent = React.forwardRef((props, ref) => {
-  function testClick() {
-    ref.current.focus();
+//Higher Order Components in React
+// class EmployeeRecords extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       employees: [],
+//     }
+//   }
+//   componentDidMount() {
+//     fetch('http://localhost:8000/api/employees').then(response => response.json()).then(result => {
+//       this.setState({ employees: result.employees })
+//     })
+//   }
+//   render() {
+//     return (<div className="container mt-3">
+//       <h4>Employees</h4>
+//       <table class="table table-striped">
+//         <thead>
+//           <tr>
+//             <th scope="col">#</th>
+//             <th scope="col">Name</th>
+//             <th scope="col">Location</th>
+//             <th scope="col">Salary</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {this.state.employees.map(employee => (
+//             <tr key={employee.id}>
+//               <th scope="row">{employee.id}</th>
+//               <th scope="row">{employee.name}</th>
+//               <td >{employee.location}</td>
+//               <td>{employee.salary}</td>
+
+//             </tr>
+//           ))}
+
+//         </tbody>
+//       </table>
+
+//     </div>)
+//   }
+// }
+
+
+// class DepartmentRecords extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       departments: [],
+//     }
+//   }
+//   componentDidMount() {
+//     fetch('http://localhost:8000/api/departments').then(response => response.json()).then(result => {
+//       this.setState({ departments: result.departments })
+//     })
+//   }
+//   render() {
+//     return (<div className="container mt-3">
+//       <h4>Departments</h4>
+//       <table class="table table-striped">
+//         <thead>
+//           <tr>
+//             <th scope="col">#</th>
+//             <th scope="col">Name</th>
+//             <th scope="col">Revenue</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {this.state.departments.map(department => (
+//             <tr key={department.id}>
+//               <th scope="row">{department.id}</th>
+//               <th scope="row">{department.name}</th>
+//               <td >{department.revenue}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>)
+//   }
+// }
+
+
+
+// small version of above components
+function reportsHOC(InputComponent, inputData) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: [],
+        columns: inputData.columns,
+        header: inputData.header,
+      }
+    }
+    componentDidMount() {
+      fetch(inputData.url).then(res => res.json()).then(result => {
+        this.setState({ data: result.data })
+      })
+    }
+    render() {
+      return (<div>
+        <Data data={this.state}>
+        </Data>
+      </div>)
+    }
   }
-  return (
-    <button onClick={testClick}>Click</button>
-  )
+}
+class Data extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {
+      this.props.data.columns.map(c => {
+        console.log(c)
+      })
+    }
+    return (
+      <div>
+        <h1>{this.props.data.header}</h1>
+        <table >
+          <thead>
+            <tr>
+              {this.props.data.columns.map((c) => {
+                (<th>{c}</th>)
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {/* {this.props.data.data.map((r) => {
+              <tr key={r.id}>
+                {this.props.data.columns.map(c => {
+                  <td>{r[c]}</td>
+                })}
+              </tr>
+            })} */}
+          </tbody>
+        </table>
+      </div >
+
+    )
+  }
+}
+class Reports extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+const EmployeeRecords = reportsHOC(Reports, {
+  url: 'http://localhost:8000/api/employees',
+  columns: ["Id", "Name", "Salary", "Location"],
+  header: "Employee Data"
 });
 
+const DepartmentRecords = reportsHOC(Reports, { url: 'http://localhost:8000/api/departments', columns: ["Id", "Name", "Revenue"], header: "Departments Data" })
 
-
-class Elevetor extends React.Component {
+class AdminDashboard extends React.Component {
 
   constructor(props) {
     super(props);
-    this.elevetorRef = React.createRef();
-  }
-  render() {
-
-    return (<div>
-      <h4>Welcome to Elevetor Ordering screen.</h4>
-      <p>
-        <label>Elevetor  Name:</label>
-        <input type="text" ref={this.elevetorRef} />
-      </p>
-      <p>
-        <label>Elevetor  Speed:</label>
-        <input type="text" />
-      </p>
-      <p>
-        <label>Elevetor  Load:</label>
-        <input type="text" />
-      </p>
-      <Summary innerRef={this.elevetorRef}></Summary>
-      <DemoComponent ref={this.elevetorRef}></DemoComponent>
-    </div>)
-  }
-}
-class Summary extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  focusInput = () => {
-    this.props.innerRef.current.focus();
   }
   render() {
     return (<div>
-      <h4>Order Summary</h4>
-      <p onClick={this.focusInput}>
-        <label>Elevetor  Name:</label>
-        <b>Product Name</b>
-      </p>
-      <p>
-        <label>Elevetor  Speed:</label>
-        <b> 10 m/s
-        </b>
-      </p>
-      <p>
-        <label>Elevetor  Load:</label>
-        <b> 550 Kg
-        </b>
-      </p>
+      <h4>Dasboard</h4>
+      <React.Fragment>
+        <EmployeeRecords></EmployeeRecords>
+        <DepartmentRecords></DepartmentRecords>
+      </React.Fragment>
     </div>)
   }
 }
 
-function testComponent() {
-  let testRef = null;
-  function handleClick() {
-    testRef.focus();
-  }
-  return (<div>
-    <input type="text" ref={e => testRef = e} />
-    <input type="button" value="Focus the test input " onClick={handleClick} />
-  </div>)
-}
 
-
-const app = <Elevetor></Elevetor>
+const app = <AdminDashboard></AdminDashboard>
 reactDom.render(app, document.getElementById('application'))
