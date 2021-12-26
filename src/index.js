43046f4f501ec22a8d5e8b1174253b1fe9d23b82
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-27 00:59:28 +0530

    Render Props in React

--- src/index.js
+++ src/index.js
@@ -4,252 +4,132 @@ import ReactDOM from "react-dom";
 
 
 
-//Profiler in React
-
-class HtmlColor extends React.Component {
-
+//Render Props in React
+class DisplayData extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return (<div>
-      <table class="table table-striped">
+    return (
+      <ul>
+        {this.props.list.map(d => (
+          <li>{d}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+
+
+class Department extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ['Dev', 'Big Data', 'Mobility']
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Department List...</h2>
+        {this.props.render(this.state.list)}
+      </div>
+    );
+  }
+}
+
+class Project extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Projects List...</h2>
+        {this.props.render(this.props.list)}
+      </div>
+    );
+  }
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Department render={(data) => <DisplayData list={data}></DisplayData>}></Department>
+        <Project render={(data) => <DisplayData list={['P-1', 'P-2', 'P-3']}></DisplayData>}></Project>
+      </React.Fragment>
+    )
+  }
+}
+
+class DisplayEmployees extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <table>
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">Color</th>
-            <th scope="col">Hexa Color</th>
-            <th scope="col">Handle</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Salary</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td><input type="color" value="#df6c4f"></input></td>
-            <td>#df6c4f</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td><input type="color" value="#49c5b6"></input></td>
-            <td>#49c5b6</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td><input type="color" value="#3c948b"></input></td>
-            <td>#3c948b</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td><input type="color" value="#1a99aa"></input></td>
-            <td>#1a99aa</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td><input type="color" value="#ecd06f"></input></td>
-            <td>#ecd06f</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">6</th>
-            <td><input type="color" value="#2779a7"></input></td>
-            <td>#2779a7</td>
-            <td>awwwards.com</td>
-          </tr>
-
-          <tr>
-            <th scope="row">7</th>
-            <td><input type="color" value="#d14836"></input></td>
-            <td>#d14836</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">8</th>
-            <td><input type="color" value="#e6eaea"></input></td>
-            <td>#e6eaea</td>
-            <td> awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">9</th>
-            <td><input type="color" value="#8154ef"></input></td>
-            <td>#8154ef</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">10</th>
-            <td><input type="color" value="#ff4e4e"></input></td>
-            <td>#ff4e4e</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">11</th>
-            <td><input type="color" value="#9fd2d6"></input></td>
-            <td>#9fd2d6</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">12</th>
-            <td><input type="color" value="#f05f70"></input></td>
-            <td>#f05f70</td>
-            <td>awwwards.com</td>
-          </tr>
-          <tr>
-            <th scope="row">13</th>
-            <td><input type="color" value="#00fca3"></input></td>
-            <td>#00fca3</td>
-            <td>awwwards.com</td>
-          </tr>
+          {this.props.employees.map(emp => (
+            <tr key={emp.id}>
+              <td>{emp.id}</td>
+              <td>{emp.name}</td>
+              <td>{emp.location}</td>
+              <td>{emp.salary}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
-
-    </div>)
-  }
-}
-
-class NewAccountReports extends React.Component {
-
-  constructor(props) {
-
-    super(props);
-
-    this.state = {
-
-      FromDate: '',
-
-      ToDate: ''
-
-    };
-
-  }
-
-
-
-  handleChange = e => {
-
-    let name = e.target.name;
-
-    let value = e.target.value;
-
-    this.setState({
-
-      [name]: value
-
-    });
-
-  }
-
-  render() {
-
-    return (
-
-      <div>
-
-        <h2>Welcome to New Accounts Reports Component...</h2>
-
-        <p>
-
-          <label>From Date : <input type="text" name="FromDate"
-
-            onChange={this.handleChange} value={this.state.FromDate}></input></label>
-
-        </p>
-
-        <p>
-
-          <label>To Date : <input type="text" name="ToDate"
-
-            onChange={this.handleChange} value={this.state.ToDate}></input></label>
-
-        </p>
-
-        <input type="submit" value="Generate"></input>
-
-      </div>
-
     )
-
   }
-
 }
 
 
-
-class LoansRepaymentReports extends React.Component {
-
+class EmployeeReports extends React.Component {
   constructor(props) {
-
     super(props);
-
+    this.state = {
+      employees: []
+    };
   }
-
-
-
+  componentDidMount() {
+    fetch("http://localhost:8000/api/employees")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            employees: result.data
+          });
+        }
+      );
+  }
   render() {
-
     return (
-
       <div>
-
-        <h2>Welcome to Loans Repayment Reports Component...</h2>
-
+        <h2>Employees Data...</h2>
+        {this.props.content(this.state.employees)}
       </div>
-
     );
-
   }
-
 }
+// const element = <Page></Page>
+const element = <EmployeeReports content=
 
+{(input)=><DisplayEmployees employees={input}></DisplayEmployees>}>                    
 
-
-class ReportsDashboard extends React.Component {
-
-  constructor(props) {
-
-    super(props);
-
-  }
-
-
-
-
-  callbackFunction = (id, phase, actualDuration, baseDuration, startTime,
-
-    commitTime, interaction) => {
-
-    console.log('Id is : ' + id + ', Phase is : ' + phase);
-
-    console.log('Actual Duration is : ' + actualDuration + ' and Base Duration is :' +
-
-      baseDuration);
-
-  }
-  render() {
-
-    return (
-
-      <React.Fragment>
-
-        <h2>Welcome to Reports Dashboard...</h2>
-
-        <Profiler id="newAccounts" onRender={this.callbackFunction}>
-
-          <NewAccountReports></NewAccountReports>
-
-        </Profiler>
-        <Profiler id="loanReplaymentReports" onRender={this.callbackFunction}>
-          <LoansRepaymentReports></LoansRepaymentReports>
-        </Profiler>
-      </React.Fragment>
-
-    );
-
-  }
-
-}
-
-const element = <ReportsDashboard></ReportsDashboard>
+</EmployeeReports>
 ReactDOM.render(element, document.getElementById('application'))
