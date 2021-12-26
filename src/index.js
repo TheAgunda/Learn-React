d98ec1eee1652b3e4b89d375dbaf53ce47e9b185
badolakiran96 <kiran.kumar@hachiweb.com>
2021-12-26 23:38:39 +0530

    Create portal in react

--- src/index.js
+++ src/index.js
@@ -1,187 +1,109 @@
 
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
 
 
 
-//Higher Order Components in React
-// class EmployeeRecords extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       employees: [],
-//     }
-//   }
-//   componentDidMount() {
-//     fetch('http://localhost:8000/api/employees').then(response => response.json()).then(result => {
-//       this.setState({ employees: result.employees })
-//     })
-//   }
-//   render() {
-//     return (<div className="container mt-3">
-//       <h4>Employees</h4>
-//       <table class="table table-striped">
-//         <thead>
-//           <tr>
-//             <th scope="col">#</th>
-//             <th scope="col">Name</th>
-//             <th scope="col">Location</th>
-//             <th scope="col">Salary</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {this.state.employees.map(employee => (
-//             <tr key={employee.id}>
-//               <th scope="row">{employee.id}</th>
-//               <th scope="row">{employee.name}</th>
-//               <td >{employee.location}</td>
-//               <td>{employee.salary}</td>
-
-//             </tr>
-//           ))}
-
-//         </tbody>
-//       </table>
-
-//     </div>)
-//   }
-// }
-
-
-// class DepartmentRecords extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       departments: [],
-//     }
-//   }
-//   componentDidMount() {
-//     fetch('http://localhost:8000/api/departments').then(response => response.json()).then(result => {
-//       this.setState({ departments: result.departments })
-//     })
-//   }
-//   render() {
-//     return (<div className="container mt-3">
-//       <h4>Departments</h4>
-//       <table class="table table-striped">
-//         <thead>
-//           <tr>
-//             <th scope="col">#</th>
-//             <th scope="col">Name</th>
-//             <th scope="col">Revenue</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {this.state.departments.map(department => (
-//             <tr key={department.id}>
-//               <th scope="row">{department.id}</th>
-//               <th scope="row">{department.name}</th>
-//               <td >{department.revenue}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>)
-//   }
-// }
-
-
-
-// small version of above components
-function reportsHOC(InputComponent, inputData) {
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        data: [],
-        columns: inputData.columns,
-        header: inputData.header,
-      }
-    }
-    componentDidMount() {
-      fetch(inputData.url).then(res => res.json()).then(result => {
-        this.setState({ data: result.data })
-      })
-    }
-    render() {
-      return (<div>
-        <Data data={this.state}>
-        </Data>
-      </div>)
-    }
-  }
-}
-class Data extends React.Component {
+//Portals in React
+class Employee extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      employees: []
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:8000/api/employees")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            employees: result.data
+          });
+        }
+      );
+  }
+  editEmployee = () => {
+    this.setState({ showModal: !this.state.showModal });
   }
   render() {
-    {
-      this.props.data.columns.map(c => {
-        console.log(c)
-      })
-    }
     return (
-      <div>
-        <h1>{this.props.data.header}</h1>
-        <table >
+      <div className="container">
+        <h2>Employees Data...</h2>
+        <table className="table table-striped">
           <thead>
             <tr>
-              {this.props.data.columns.map((c) => {
-                (<th>{c}</th>)
-              })}
+              <th>Id</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Salary</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            {/* {this.props.data.data.map((r) => {
-              <tr key={r.id}>
-                {this.props.data.columns.map(c => {
-                  <td>{r[c]}</td>
-                })}
+            {this.state.employees.map(emp => (
+              <tr key={emp.id}>
+                <td>{emp.id}</td>
+                <td>{emp.name}</td>
+                <td>{emp.location}</td>
+                <td>{emp.salary}</td>
+                <td>
+                  <button onClick={this.editEmployee} className="btn btn-sm btn-danger">Edit</button>
+                  <Modal open={this.state.showModal} close={this.editEmployee}>
+                    <EmployeeModal employee={emp} name={emp.name}></EmployeeModal>
+                  </Modal>
+                </td>
               </tr>
-            })} */}
+            ))}
           </tbody>
         </table>
-      </div >
-
-    )
+      </div>
+    );
   }
 }
-class Reports extends React.Component {
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      this.props.open ? ReactDOM.createPortal(
+        <div className="container">
+          <div className="card mt-3">
+            <div className="card-body">
+              <button onClick={this.props.close} type="button" className="btn-close" aria-label="Close"></button>
+              {this.props.children}
+            </div></div></div>, document.body) : null
+    );
+  }
+}
+
+class EmployeeModal extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
+        <h5 class="card-title">{this.props.name} Details...</h5>
+        <p>
+          <label>Employee ID : <input type="text" className="form-control form-control-sm" value={this.props.employee.id}></input></label>
+        </p>
+        <p>
+          <label>Employee Name : <input type="text" className="form-control form-control-sm" value={this.props.employee.name}></input></label>
+        </p>
+        <p>
+          <label>Employee Location : <input type="text" className="form-control form-control-sm" value={this.props.employee.location}></input></label>
+        </p>
+        <p>
+          <label>Employee Salary : <input type="text" className="form-control form-control-sm" value={this.props.employee.salary}></input></label>
+        </p>
+        <input type="submit" value="Save" className="btn btn-sm btn-primary"></input>
       </div>
     )
   }
 }
-const EmployeeRecords = reportsHOC(Reports, {
-  url: 'http://localhost:8000/api/employees',
-  columns: ["Id", "Name", "Salary", "Location"],
-  header: "Employee Data"
-});
-
-const DepartmentRecords = reportsHOC(Reports, { url: 'http://localhost:8000/api/departments', columns: ["Id", "Name", "Revenue"], header: "Departments Data" })
-
-class AdminDashboard extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (<div>
-      <h4>Dasboard</h4>
-      <React.Fragment>
-        <EmployeeRecords></EmployeeRecords>
-        <DepartmentRecords></DepartmentRecords>
-      </React.Fragment>
-    </div>)
-  }
-}
 
 
-const app = <AdminDashboard></AdminDashboard>
-reactDom.render(app, document.getElementById('application'))
+const app = <Employee></Employee>
+ReactDOM.render(app, document.getElementById('application'))
